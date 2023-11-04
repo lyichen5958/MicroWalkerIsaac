900935aa17e31974149ef08e750adcf52a479efc
Felipe Campos <fmrcampos@berkeley.edu>
2023-11-04 09:27:05 -0700

    "Init commit to github with modification on microbot on Hiro"

---
+++ microquad_ros/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/hiro/IsaacGymV4/microquad_ros/catkin_ws/src
+# Build directory: /home/hiro/IsaacGymV4/microquad_ros/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(_ctest_urdf_tutorial_roslaunch-check_launch "/home/hiro/IsaacGymV4/microquad_ros/catkin_ws/build/catkin_generated/env_cached.sh" "/usr/bin/python2" "/opt/ros/melodic/share/catkin/cmake/test/run_tests.py" "/home/hiro/IsaacGymV4/microquad_ros/catkin_ws/build/test_results/urdf_tutorial/roslaunch-check_launch.xml" "--return-code" "/usr/bin/cmake -E make_directory /home/hiro/IsaacGymV4/microquad_ros/catkin_ws/build/test_results/urdf_tutorial" "/opt/ros/melodic/share/roslaunch/cmake/../scripts/roslaunch-check -o \"/home/hiro/IsaacGymV4/microquad_ros/catkin_ws/build/test_results/urdf_tutorial/roslaunch-check_launch.xml\" \"/home/hiro/IsaacGymV4/microquad_ros/catkin_ws/src/launch\" ")
+subdirs("gtest")
---
+++ microquad_ros/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH='/opt/ros/melodic/lib:/opt/ros/eloquent/opt/yaml_cpp_vendor/lib:/opt/ros/eloquent/opt/rviz_ogre_vendor/lib:/opt/ros/eloquent/lib/x86_64-linux-gnu:/opt/ros/eloquent/lib:/usr/local/cuda-9.0/lib64:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/usr/lib/nvidia:/home/hiro/.mujoco/mujoco210/bin'
+export PWD='/home/hiro/IsaacGymV4/microquad_ros/catkin_ws/build'
+export ROS_PACKAGE_PATH='/home/hiro/IsaacGymV4/microquad_ros/catkin_ws/src:/opt/ros/melodic/share'
